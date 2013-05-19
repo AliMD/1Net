e5956078335d0f293eea2e7424ba51de351a8784
Ali Mihandoost <i@ali.md>
2013-05-19 14:51:14 +0430

    Test for console logs

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 /*jshint strict:true, es5:true, forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:true, undef:true, unused:true, nonew:true, browser:true, devel:true, indent:2, boss:true, curly:false, immed:false, latedef:true, newcap:true, plusplus:false, trailing:true, maxerr:100, debug:false, asi:false, evil:false, expr:true, eqnull:false, esnext:false, funcscope:false, globalstrict:false, loopfunc:false */
 (function($,undefined){
   "use strict";
-  console.group('App.main');
+  console.group("%c1Net %o","color:darkmagenta; padding-left:15px","http://ali.md/1net");
+  console.group('main.js');
 
   var
-
     initTime = Date.now(),
 
     log = function (msg) {
@@ -12,10 +12,11 @@
     },
 
     init = function () {
-      log("App Start @ "+initTime);
-    }
+      log("Initialize @ "+initTime);
+    };
 
   init();
 
+  console.groupEnd();
   console.groupEnd();
 })(window.jQuery);
